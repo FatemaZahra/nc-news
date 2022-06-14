903a855ca813cda53638e5f1d4b9dfb58f9086f6
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-06-14 11:44:51 +0100

    Basic CSS

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 const ArticleCard = ({ article }) => {
   return (
     <div className="ArticleCard">
-      <Link to={`/articles/${article.article_id}`}>
-        <h2>{article.topic}</h2>
+      <Link to={`/articles/${article.article_id}`} className="link">
+        <h2>
+          {article.topic[0].toUpperCase() +
+            article.topic.slice(1, article.length)}
+        </h2>
         <h1>{article.title}</h1>
         <h3>{article.author}</h3>
         <p>{article.body}</p>
