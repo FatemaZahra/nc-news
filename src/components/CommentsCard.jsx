0c6e955b1d8bb7730d6d88efb0148b7f8b67033d
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-06-15 10:10:35 +0100

    Changed comment card structure

--- src/components/CommentsCard.jsx
+++ src/components/CommentsCard.jsx
@@ -2,13 +2,13 @@ const CommentsCard = ({ comment }) => {
   return (
     <div className="CommentsCard">
       <h4>{comment.author}</h4>
-      <p>{comment.body}</p>
-      <p>
+      <span>
         <datetime className="date">
           {comment.created_at.slice(0, 10)},{" "}
         </datetime>
         <time className="date">{comment.created_at.slice(11, 16)}</time>
-      </p>
+      </span>
+      <p>{comment.body}</p>
       <p>Votes on Comment: {comment.votes}</p>
     </div>
   );
